71d31e5bff5b33b24068802e72ce3ecadd82f666
Andrew <andepopeskua@gmail.com>
2024-01-13 16:22:04 +0200

    changed namespace

---
+++ Schedoo.Server/Helpers/ScheduleWrapper.cs
@@ -0,0 +1,23 @@
+﻿using Schedoo.Server.Models;
+
+namespace Schedoo.Server.Helpers
+{
+    public class ScheduleWrapper
+    {
+        public class ScheduleCat
+        {
+            public List<Schedule> OddWeekClasses { get; set; }
+            public List<Schedule> EvenWeekClasses { get; set; }
+
+            public ScheduleCat(List<Schedule> oddWeekClasses, List<Schedule> evenWeekClasses)
+            {
+                OddWeekClasses = oddWeekClasses;
+                EvenWeekClasses = evenWeekClasses;
+            }
+        }
+
+        public ScheduleCat ScheduleAll { get; set; }
+        public List<string> Days { get; set; }
+        public SortedSet<TimeSlot> TimeSlots { get; set; }
+    }
+}
