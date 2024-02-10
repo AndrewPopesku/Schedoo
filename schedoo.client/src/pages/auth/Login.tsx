f6e67a1171c3d5c7d6c413449d6d35b790f0cf82
Andrew <andepopeskua@gmail.com>
2024-02-10 22:11:48 +0200

    added enums file

---
+++ schedoo.client/src/pages/auth/Login.tsx
@@ -0,0 +1,101 @@
+import { useEffect, useRef, useState } from "react"
+import axios from '../../api/axios'
+import { useAuth } from "../../hooks/useAuth";
+import { useLocation, useNavigate } from "react-router-dom";
+
+const LOGIN_URL = "/login";
+
+export function Login() {
+    const { setAuth } = useAuth();
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    const emailRef = useRef<HTMLInputElement>(null);
+    const errRef = useRef<HTMLParagraphElement>(null);
+
+    const [email, setEmail] = useState<string>('');
+    const [pwd, setPwd] = useState<string>('');
+    const [errMsg, setErrMsg] = useState<string>('');
+
+    useEffect(() => {
+        emailRef.current?.focus();
+    }, []);
+
+    useEffect(() => {
+        setErrMsg('');
+    }, [email, pwd]);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await axios.post(LOGIN_URL,
+                JSON.stringify({
+                    email: email,
+                    password: pwd
+                }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            const accessToken = response?.data?.accessToken;
+            const refreshToken = response?.data?.refreshToken;
+            setAuth({ email, pwd, accessToken, refreshToken });
+            setEmail('');
+            setPwd('');
+            console.log("Navigating to", from);
+            navigate(from, { replace: true });
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 400) {
+                setErrMsg('Missing Username or Password');
+            } else if (err.response?.status === 401) {
+                setErrMsg('Unauthorized');
+            } else {
+                setErrMsg('Login Failed');
+            }
+            errRef.current.focus();
+        }
+    }
+
+    return (
+        <section>
+            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
+            <h1>Sign In</h1>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="email">Email:</label>
+                <input
+                    type="text"
+                    id="email"
+                    ref={emailRef}
+                    autoComplete="off"
+                    onChange={(e) => setEmail(e.target.value)}
+                    value={email}
+                    required
+                />
+
+                <label htmlFor="password">Password:</label>
+                <input
+                    type="password"
+                    id="password"
+                    onChange={(e) => setPwd(e.target.value)}
+                    value={pwd}
+                    required
+                />
+                <button>Sign In</button>
+            </form>
+            <p>
+                Need an Account?<br />
+                <span className="line">
+                    {/*put router link here*/}
+                    <a href="#">Sign Up</a>
+                </span>
+            </p>
+        </section>
+
+    )
+}
